a86f1295e6ad618d199ac076a99f26c69fe04817
Coaxial <thecoaxial@gmail.com>
2021-01-12 17:14:27 -0500

    Fixed issue where saved notes wouldn't display, also fixed delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,57 @@
 // Require fs
 const fs = require('fs');
-
+let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app){
-
-    fs.readFile("db/db.json","utf8", (err,data) => {
-        if (err) throw err;
-
-        let db = JSON.parse(data);
-
+        
         //Get route
         app.get("/api/notes", function(req, res){
-            res.json(JSON.parse(db));
+            res.json(db);
+            
+        });
+
+         // Should Retrieve a note with specific id
+         app.get("/api/notes/:id", function(req,res) {           
             res.json(db[req.params.id]);
         });
 
         //Post route
         app.post("/api/notes", function(req, res){
-            let id = db.length + 1;
+            let id = db.length;
             let newNote = { title: req.body.title, text: req.body.text, id: id }; 
             db.push(newNote);
             updateNotes();
+            res.json(db);
            
 
             return console.log("Added note " + newNote.title);
-        })
+        });
+
+       
+
+         //Should Delete a note with specific id
+         app.delete("/api/notes/:id", function(req, res) {
+           
+            let newId = 0;
+            
+            db = db.filter(currentNote => {
+            return currentNote.id !=  req.params.id;
+        });
+            for (currentNote of db) {
+            currentNote.id = newId.toString();
+            newId++;
+        };
+            updateNotes();
+            
+            res.json(db);
+        });
 
         function updateNotes() {
             fs.writeFile("db/db.json", JSON.stringify(db),err =>{
-                    if (err) throw err;    
+                if (err) throw err;    
             });
         }
 
-    })
+    
 
 }
